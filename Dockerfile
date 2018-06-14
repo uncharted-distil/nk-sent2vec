d9d69bae0928434fae7dafb18390929d22556d95
Craig Corcoran <craig@newknowledge.io>
2018-06-14 09:01:54 -0500

    make setup.py install sent2vec, set default dockerfile to just do setup.py install, move doing it in dockerfile to Dockerfile-dev

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,8 @@
-FROM registry.datadrivendiscovery.org/jpl/docker_images/complete:ubuntu-artful-python36-devel-20180419-092215
+FROM registry.datadrivendiscovery.org/jpl/docker_images/complete:ubuntu-artful-python36-v2018.6.5
 
-ENV HOME=/ibex
+ENV HOME=/nk_sent2vec
 
 WORKDIR $HOME
-
-# copy files that define dependencies so we can have package installs in a separate image layer 
-COPY requirements.txt $HOME/
-# install required packages on pypi
-RUN pip3 install -r requirements.txt
-
-# Install set2vec: clone from git repo, compile binary, then install python bindings
-RUN git clone https://github.com/epfml/sent2vec.git $HOME/sent2vec \
-    && rm -rf $HOME/sent2vec/.git* \
-    && cd $HOME/sent2vec \
-    && make \
-    && cd $HOME/sent2vec/src \
-    && python3 setup.py build_ext \
-    && pip3 install . 
 
 # install nk_sent2vec
 COPY . $HOME/
--- setup.py
+++ setup.py
@@ -1,4 +1,31 @@
+import os
+import setuptools.command.develop as develop_cmd
+import setuptools.command.install as install_cmd
 from setuptools import setup
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+S2V_INSTALL_PATH = os.getenv('S2V_INSTALL_PATH', os.path.join(current_dir, 'sent2vec'))
+
+
+class install(install_cmd.install):
+    ''' Build sent2vec/FastText c++ binary, then pip install sent2vec. '''
+
+    def run(self):
+        install_cmd.install.run(self)
+        os.system("git clone https://github.com/epfml/sent2vec.git {0}".format(S2V_INSTALL_PATH))
+        os.system("cd {0} && make".format(S2V_INSTALL_PATH))
+        os.system("cd {0}/src && python setup.py build_ext && pip install . ".format(S2V_INSTALL_PATH))
+
+
+class develop(develop_cmd.develop):
+    ''' Build sent2vec/FastText c++ binary, then pip install sent2vec. '''
+
+    def run(self):
+        develop_cmd.develop.run(self)
+        os.system("git clone https://github.com/epfml/sent2vec.git {0}".format(S2V_INSTALL_PATH))
+        os.system("cd {0} && make".format(S2V_INSTALL_PATH))
+        os.system("cd {0}/src && python setup.py build_ext && pip install . ".format(S2V_INSTALL_PATH))
+
 
 setup(
     name='nk_sent2vec',
@@ -6,10 +33,15 @@ setup(
     description='Embeds text documents using sent2vec',
     author='New Knowledge',
     packages=['nk_sent2vec'],
+    package_data={'': ['*.h, *.cc', '*Makefile']},
     include_package_data=True,
     install_requires=[
         'Cython>=0.28.3',
         'numpy>=1.14.1',
         'nose>=1.3.7',
     ],
+    cmdclass={
+        'install': install,
+        'develop': develop,
+    }
 )
