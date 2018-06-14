a813fbd5159e0a9011f224f42cb8e41ed1ae857a
Craig Corcoran <craig@newknowledge.io>
2018-06-14 10:28:22 -0500

    move s2v install into setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,34 @@
+import os
+
 from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+S2V_INSTALL_PATH = '/app/epfml_sent2vec'
+
+
+class InstallSent2Vec(install):
+    ''' Build sent2vec FastText binary, then pip install sent2vec. '''
+
+    def run(self):
+        install.do_egg_install(self)  # instead of install.run(self)
+        os.system("git clone https://github.com/epfml/sent2vec.git {0}".format(S2V_INSTALL_PATH))
+        os.system("cd {0} && make".format(S2V_INSTALL_PATH))
+        os.system("cd {0}/src && python3 setup.py build_ext && pip3 install . ".format(S2V_INSTALL_PATH)
+                  )
+
+
+class DevelopSent2Vec(develop):
+    ''' Clone epfml's sent2vec repo, build sent2vec FastText binary, and pip install sent2vec. '''
+
+    def run(self):
+        # install.run(self) # instead of develop.run(self)
+
+        develop.do_egg_install(self)
+        os.system("git clone https://github.com/epfml/sent2vec.git {0}".format(S2V_INSTALL_PATH))
+        os.system("cd {0} && make".format(S2V_INSTALL_PATH))
+        os.system("cd {0}/src && python3 setup.py build_ext && pip3 install . ".format(S2V_INSTALL_PATH))
+
 
 setup(
     name='nk_sent2vec',
@@ -6,10 +36,15 @@ setup(
     description='Embeds text documents using sent2vec',
     author='New Knowledge',
     packages=['nk_sent2vec'],
+    package_data={'': ['*.h, *.cc', '*Makefile']},
     include_package_data=True,
     install_requires=[
         'Cython>=0.28.3',
         'numpy>=1.14.1',
         'nose>=1.3.7',
     ],
+    cmdclass={
+        'install': InstallSent2Vec,
+        'develop': DevelopSent2Vec,
+    }
 )
