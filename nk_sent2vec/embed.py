128e801d7afffa0be1ba8ac3615903726345649d
azunre <azunre@PaulLinuxGPUDev.jaavmej2qrwupdw4djg0r2u5ud.bx.internal.cloudapp.net>
2018-09-18 19:17:46 +0000

    fixing space/tab issue with vscode

--- nk_sent2vec/embed.py
+++ nk_sent2vec/embed.py
@@ -14,28 +14,28 @@ import sent2vec
 class Sent2Vec:
 	def __init__(self, path):
 		model = sent2vec.Sent2vecModel()
-        	model.load_model(path)
-        	self.model = model
+        model.load_model(path)
+        self.model = model
 
 	def produce(self, sentences: List[str]):
-    		return self.model.embed_sentences(sentences)
+    	return self.model.embed_sentences(sentences)
 
 	def embed_sentence(self, sentence: str):
-    		return self.model.embed_sentences(sentence)
+    	return self.model.embed_sentences(sentence)
 
 	def embed_sentences(self, sentences: List[str]):
-   		 return self.model.embed_sentences(sentences)
+   		return self.model.embed_sentences(sentences)
 
 	def train_model(self, input_path, output_path):
    		# TODO
-    		pass
+    	pass
 
 	def nearest_neighbors(self, query_sentence, corpus_path):
-    		# ./fasttext nnSent model.bin corpora [k]
-    		# TODO
-    		pass
+    	# ./fasttext nnSent model.bin corpora [k]
+    	# TODO
+    	pass
 
 	def analogies(self, query_sentence, corpus_path):
-    		# ./fasttext analogiesSent model.bin corpora [k]
-    		# TODO
-    		pass
+    	# ./fasttext analogiesSent model.bin corpora [k]
+    	# TODO
+    	pass
