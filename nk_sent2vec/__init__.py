a5da90b4e61d4b538bd6724b7d3c0bf07cbe1825
root <root@numa-vm.34acs2fjacxunc01gwkw1ffa0g.jx.internal.cloudapp.net>
2018-09-18 19:44:35 +0000

    fixing init file

--- nk_sent2vec/__init__.py
+++ nk_sent2vec/__init__.py
@@ -1,1 +1,6 @@
-from .embed import embed_sentences, model, train_model, nearest_neighbors, analogies, produce
+# from .embed import embed_sentences, model, train_model, nearest_neighbors, analogies, produce
+from nk_sent2vec.embed import Sent2Vec 
+
+__version__ = '1.2.0'
+
+__all__ = ['Sent2Vec'] 
