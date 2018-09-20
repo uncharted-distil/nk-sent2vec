571f2e9dcf64b340e57f6750610b08463f24365a
root <numadhamani@gmail.com>
2018-09-20 18:20:32 +0000

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
-from setuptools import setup
+# from setuptools import setup
+from distutils.core import setup
 from setuptools.command.develop import develop
 from setuptools.command.install import install
 
