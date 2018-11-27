f45559d581d491112f19f82f9b316e250663c9aa
Numa Dhamani <numadhamani@gmail.com>
2018-11-27 11:37:23 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-# import os
+import os
 from distutils.core import setup
-# from setuptools.command.develop import develop
-# from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools.command.install import install
 
 # S2V_INSTALL_PATH = '/app/epfml_sent2vec'
 
